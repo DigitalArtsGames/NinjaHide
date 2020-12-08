19e148f053f29f1b53d243766a8150ca68024e8a
DESKTOP-2KM365Q\Mustafa <metro9952@gmail.com>
2020-12-08 20:42:13 +0400

    New FOV added

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -9,6 +9,47 @@ public class FieldOfView : MonoBehaviour
     [Range (0, 360)]
     public float viewAngle;
 
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirTotarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirTotarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                
+                if(!Physics.Raycast(transform.position, dirTotarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                    Debug.Log("I see you!");
+                }
+            }
+        }
+    }
+
     public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         if(!angleIsGlobal)
