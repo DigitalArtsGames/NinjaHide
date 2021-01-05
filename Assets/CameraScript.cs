c5c5617d4181226a8964f4e2bbfcd329f89baf9a
DESKTOP-2KM365Q\Mustafa <metro9952@gmail.com>
2021-01-05 18:53:35 +0400

    Attemp #2

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Cinemachine.CinemachineVirtualCamera cinemaVirCam;
+
+    void Update()
+    {
+        if(cinemaVirCam.Follow == null)
+            cinemaVirCam.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+}
--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class BillboardScript : MonoBehaviour
 {
     //Не критичные предупреждения, потом нужно пофиксить
-#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+
     public Transform camera;
-#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
 
     void Update()
     {
-        transform.LookAt(transform.position + camera.forward);
+        if(camera != null)
+        {
+            transform.LookAt(transform.position + camera.forward);
+        } else
+        {
+            camera = GameObject.FindGameObjectWithTag("Camera").transform;
+        }
     }
 }
