ddcbefd68c28ca2343f77a88b58a790e4a75082d
Mustafa <45076611+MustafaThebest@users.noreply.github.com>
2021-01-19 12:15:35 +0400

    New Menu

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject gameCamera;
+    public GameObject mainMenuCamera;
+
+    public void Start()
+    {
+        SwitchToMainMenuCamera();
+    }
+
+    public void SwitchToGameCamera()
+    {
+        mainMenuCamera.SetActive(false);
+        gameCamera.SetActive(true);
+        //Follow
+    }
+    
+    public void SwitchToMainMenuCamera()
+    {
+        gameCamera.SetActive(false);
+        mainMenuCamera.SetActive(true);
+    }
+}
