728df070c3d9137841790dd4fa637ea0c798b3ac
Mustafa <45076611+MustafaThebest@users.noreply.github.com>
2021-01-22 10:53:40 +0400

    Magnitued Fixed

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -8,6 +8,8 @@ public class AnimationController : MonoBehaviour
     [SerializeField] private SplineWalker splineWalker;
     [SerializeField] private PlayerScript playerScript;
 
+    float velocityX;
+    float velocityZ;
 
     private Vector3 playerDirection;
     private void Update()
@@ -22,10 +24,11 @@ public class AnimationController : MonoBehaviour
         //playerDirection = PlayerScript.splineWalker.GetPlayerDirection();
         if (playerScript.GetTarget(10) != null && !playerScript.isHiding)
         {
-            playerDirection = playerScript.GetPlayerDirection(10);
+            playerDirection = playerScript.GetPlayerDirection(10) * 15;
+            
             //Debug.Log("Direction to Enemy!");
         }
-        else if(playerScript.isHiding)
+        else if (playerScript.isHiding)
         {
             playerDirection = playerScript.GetHidingSpotDirection();
             playerScript.transform.rotation = Quaternion.Euler(0, -90, 0);
@@ -36,13 +39,20 @@ public class AnimationController : MonoBehaviour
             playerDirection = transform.InverseTransformDirection(playerDirection);
         }
 
-        float velocityX = playerDirection.x * Time.deltaTime * 1000;
-        float velocityZ = playerDirection.z * Time.deltaTime * 1000;
+        //float velocityX = playerDirection.x * Time.deltaTime * 1000;
+        //float velocityZ = playerDirection.z * Time.deltaTime * 1000;
 
         //print(velocityX.ToString() + " " + velocityZ.ToString());
 
+        playerDirection = Vector3.Normalize(playerDirection);
+
+        velocityX = playerDirection.x;
+        velocityZ = playerDirection.z;
+
+
         animationController.SetFloat("MoveX", velocityX);
         animationController.SetFloat("MoveZ", velocityZ);
+
 
     }
 
