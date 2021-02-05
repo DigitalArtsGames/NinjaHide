6f719993ae1d97ecf30a8346aba803ab9960d8c7
Mustafa <45076611+MustafaThebest@users.noreply.github.com>
2021-02-05 14:15:12 +0400

    In Progress

--- Assets/Levels/LevelData.cs
+++ Assets/Levels/LevelData.cs
@@ -8,13 +8,5 @@ public class LevelData : ScriptableObject
     public new string name;
     public Room[] rooms;
 
-    private List<Room> spawnedRooms = new List<Room>();
-    
-    public void SpawnRandomRoom()
-    {
-        Room newRoom = Instantiate(rooms[Random.Range(0, rooms.Length)]);
-        //newRoom.transform.position = ;
-        spawnedRooms.Add(newRoom);
-    }
-
+    public PlayerScript player;
 }
